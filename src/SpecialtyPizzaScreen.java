619ff7aaf2e447fe343ff4be1abd2ac26c570ef2
Thomas VanderBruggen <tomvandy605@gmail.com>
2020-12-13 11:12:15 -0600

    Cleaned up a comment

--- src/SpecialtyPizzaScreen.java
+++ src/SpecialtyPizzaScreen.java
@@ -25,24 +25,6 @@ import javax.swing.JTextPane;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 
-/**************************************************************
-* Name        : 
-* Author      : 
-* Created     :
-* Course      : CIS 152 Data Structures
-* Version     : 1.0
-* OS          : Windows 10
-* Copyright   : This is my own original work based on
-*               specifications issued by our instructor
-* Description : This program overall description here
-*               Input: 
-*               Output:
-* Academic Honesty: I attest that this is my original work.
-* I have not used unauthorized source code, either modified or 
-* unmodified. I have not given other fellow student(s) access to
-* my program.         
-***************************************************************/
-
 /**
  * @author Tom
  *
